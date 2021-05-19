ae8e5f102bb2182b4d0d08e034ae3476300e3c07
sungkong <tjddnths@naver.com>
2021-05-19 22:31:37 +0900

    싱글톤패턴

---
+++ javaStudy/src/Car.java
@@ -0,0 +1,22 @@
+
+
+public class Car {
+	
+		
+		private static int serialNum = 10000;
+		private int carNum;
+		
+		public Car() {
+			serialNum++;
+			carNum = serialNum;
+		}
+		
+		public int getCarNum() {
+			return carNum;
+		}
+
+		public void createCarNum(int carNum) {
+			this.carNum = carNum;
+		}
+	
+}
